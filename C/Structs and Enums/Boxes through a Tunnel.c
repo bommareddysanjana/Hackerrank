fd79f11bfb7de951cf288bc55f28eb1011addf08
bommareddysanjana <152767998+bommareddysanjana@users.noreply.github.com>
2023-12-03 15:20:51 +0530

    Create Boxes through a Tunnel.c

---
+++ C/Structs and Enums/Boxes through a Tunnel.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_HEIGHT 41
+struct box
+{
+	int length, width , height;
+};
+
+typedef struct box box;
+
+int get_volume(box b) {
+	return (b.height * b.length * b.width);
+}
+
+int is_lower_than_max_height(box b) {
+	if (b.height < MAX_HEIGHT)
+        return 1;
+    else
+        return 0;
+}
+
